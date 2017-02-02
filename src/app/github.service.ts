240ccf7f5ea3926d878db5f0af645ca305e9161e
Tati <tatiestrada@me.com>
2017-02-02 12:23:55 +0000

    step 11

---
+++ src/app/github.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class GithubService {
+
+  constructor() { }
+
+}
+ import { Http, Headers} from '@angular/http';
+ import 'rxjs/add/operator/map';
