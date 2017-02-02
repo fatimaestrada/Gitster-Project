a2ab4bc871a5391874a76ffbd5272b09ddb35322
Tati <tatiestrada@me.com>
2017-02-02 12:32:39 +0000

    step 12 client setup

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -8,3 +8,14 @@ export class GithubService {
 }
  import { Http, Headers} from '@angular/http';
  import 'rxjs/add/operator/map';
+
+ @Injectable()
+ export class GithubService {
+
+ 	private userName:string;
+ 	private client_id:string = 'c1c326ee8dab030018d5';
+ 	private client_secret:string = '0d1050e79f3b3b0d7490938e862cb82777e16823';
+
+ constructor () {}
+ 	
+ }
