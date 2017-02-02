a83cd09562f5e1fb15f92175d8af5f1acc9a7094
Tati <tatiestrada@me.com>
2017-02-02 12:43:37 +0000

    step 14

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -16,6 +16,19 @@ export class GithubService {
  	private client_id:string = 'c1c326ee8dab030018d5';
  	private client_secret:string = '0d1050e79f3b3b0d7490938e862cb82777e16823';
 
- constructor () {}
- 	
+ constructor (private_http:Http) {
+ 	console.log('Github API service ready!');
+
+ this.userName = "pndrckr"
+
+ getUser () {
+ 	return this._http.get('http://api.github.com/users/'
+ 		+ this.userName
+ 		+ '?client_id=' + this.client_id
+ 		+'&client_secret=' + this.client_secret
+ 		).map(res => res.json());
  }
+
+ } // githubservice
+ 	
+ } // general
